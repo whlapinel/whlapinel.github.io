65bb0e057d29490c3cde5fae3d1147e19afdd8e0
Will Lapinel <whlapinel@gmail.com>
2024-09-02 01:07:28 -0400

    css issues

--- cmd/templates/layout_templ.go
+++ cmd/templates/layout_templ.go
@@ -26,7 +26,7 @@ func Layout() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Will Lapinel's Portfolio</title><link rel=\"stylesheet\" href=\"./styles/styles.css\"><script src=\"./js/htmx.js\" defer></script></head><body class=\"bg-slate-800\" hx-boost=\"true\"><header><a href=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Will Lapinel's Portfolio</title><link rel=\"stylesheet\" href=\"/styles/styles.css\"><script src=\"/js/htmx.js\" defer></script></head><body class=\"bg-slate-800\" hx-boost=\"true\"><header><a href=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
