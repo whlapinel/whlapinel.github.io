3bbf540ba36b5b842c8e5064bf7d1913148d4b19
Will Lapinel <whlapinel@gmail.com>
2024-11-28 11:40:50 -0500

    auto-sync

--- cmd/templates/courses_templ.go
+++ cmd/templates/courses_templ.go
@@ -815,7 +815,7 @@ func LessonComponent(lesson domain.Lesson, unit domain.Unit, course domain.Cours
 					if templ_7745c5c3_Err != nil {
 						return templ_7745c5c3_Err
 					}
-					if hasFilesDir(filesRoutePath(lesson, unit, course)) {
+					if hasFilesDir(lessonRoutePath(lesson, unit, course)) {
 						templ_7745c5c3_Var43 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 							templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 							templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
