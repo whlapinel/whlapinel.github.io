9f905f3217bee78d2b16f558f4481e4b2dd4a191
Will Lapinel <whlapinel@gmail.com>
2024-12-14 14:49:49 -0500

    auto-sync

--- cmd/gen_schedule/main.go
+++ cmd/gen_schedule/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"gh_static_portfolio/cmd/data"
 	"log"
+	"time"
 )
 
 func main() {
-	courseInstances, err := data.GenerateInstancesFromCSV()
+	courseInstances, err := data.GenerateCourseInstancesFromCSV2(time.Now())
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = data.WriteInstancesToCSV(courseInstances)
+	err = data.WriteCourseInstancesToCSV(courseInstances)
 	if err != nil {
 		log.Fatalf("error writing schedule to csv, %v", err)
 	}
