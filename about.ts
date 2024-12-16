a5e5179f2a0001a2667437e04ea102354641967e
Will Lapinel <whlapinel@gmail.com>
2024-12-15 20:18:41 -0500

    move course manager out

--- about.ts
+++
@@ -1,9 +0,0 @@
-(() => {
-    const collapsibles = document.querySelectorAll('.collapsible')
-    for (const collapsible of collapsibles) {
-        collapsible.addEventListener('click', () => {
-            collapsible.nextElementSibling?.classList.toggle('max-h-0')
-            collapsible.nextElementSibling?.classList.toggle('max-h-fit')
-        })
-    }
-})();
--- cmd/course_manager/templates/common.go
+++
@@ -1,9 +0,0 @@
-package templates
-
-import "github.com/a-h/templ"
-
-type Page struct {
-	Title     string
-	Route     string
-	Component templ.Component
-}
